821b0095a30572887eecd11a43948f25a9936ea6
sathish-entri <abishsathish2002@example.com>
2024-10-05 17:50:54 +0530

    chamge name

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/preloader/Preloader";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/footer/Footer";
-import { ThemeContext } from "./Context/theme";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Skills from "./components/Skills/Skills";
-import Contact from "./components/contact/Contact";
+// import Preloader from "../src/components/preloader/Preloader";
+// import Navbar from "./components/navbar/Navbar";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Projects from "./components/Projects/Projects";
+// import Footer from "./components/footer/Footer";
+// import { ThemeContext } from "./Context/theme";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Navigate,
+// } from "react-router-dom";
+// import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Skills from "./components/Skills/Skills";
+// import Contact from "./components/contact/Contact";
 
 function App() {
   const [load, upadateLoad] = useState(true);
-  const [{ themename }] = React.useContext(ThemeContext);
+  // const [{ themename }] = React.useContext(ThemeContext);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     upadateLoad(false);
+  //   }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
-    <div className={`${themename} app`}>
-      <Router>
-        <Preloader load={load} />
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
-          <Navbar />
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/project" element={<Projects />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+    // <div className={`${themename} app`}>
+    //   <Router>
+    //     <Preloader load={load} />
+    //     <div className="App" id={load ? "no-scroll" : "scroll"}>
+    //       <Navbar />
+    //       <ScrollToTop />
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/project" element={<Projects />} />
+    //         <Route path="/skills" element={<Skills />} />
+    //         <Route path="/contact" element={<Contact />} />
+    //         <Route path="/about" element={<About />} />
+    //         <Route path="*" element={<Navigate to="/" />} />
+    //       </Routes>
+    //       <Footer />
+    //     </div>
+    //   </Router>
+    // </div>
+    <div>
+      <h2>helo woeld</h2>
     </div>
   );
 }
