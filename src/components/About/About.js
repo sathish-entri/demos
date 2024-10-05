19b43f4e5dcd898c8d0e19d986867eb7c449231f
sathish-entri <abishsathish2002@example.com>
2024-10-05 18:35:03 +0530

    change some text

---
+++ src/components/About/About.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+
+import "./about.css";
+import Home2 from "../Home/Home2";
+ 
+function About() {
+  return (
+    <Container fluid className="about-section">
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Home2 />
+          {/* <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <h1
+              style={{ fontSize: "2.1em", paddingBottom: "20px" }}
+              data-aos="fade-right"
+            >
+              <span className="primary-header">Who</span> I'M
+            </h1>
+            <div data-aos="fade-up">
+              <Aboutcard />
+            </div>
+          </Col> */}
+          {/* <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          >
+            <img
+              src={laptopImg}
+              alt="about"
+              className="img-fluid"
+              data-aos="fade-left"
+            />
+          </Col> */}
+        </Row>
+        {/* <h1 data-aos="fade-right">
+          <span className="primary-header">Skillset</span> I Work With
+        </h1>
+
+        <div data-aos="fade-up">
+          <Techstack />
+        </div>
+
+        <h1 data-aos="fade-right">
+          <span className="primary-header">Tools</span> I use
+        </h1>
+        <div data-aos="fade-up">
+          <Toolstack data-aos="fade-up" />
+        </div> */}
+
+        {/* <Github /> */}
+      </Container>
+      <ScrollToTop />
+    </Container>
+  );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { ThemeProvider } from "./Context/theme";
+import { ThemeProvider } from "./Context/theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  {/* <ThemeProvider> */}
+  <ThemeProvider>
     <App />
-  {/* </ThemeProvider> */}
+  </ThemeProvider>
 </React.StrictMode>
 );
 
